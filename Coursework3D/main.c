0488a8b4dc9f5ae43de9cf85cc8c7bd9bafd9d81
陈子谦 <chenziqian@charrr.local>
2017-04-20 08:37:14 +0800

    Initial Commit

---
+++ Coursework3D/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Coursework3D
+//
+//  Created by 陈子谦 on 20/04/2017.
+//  Copyright © 2017 Charrr. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
